04721f9f257981caf8662ac854b69e39923c09ce
madalinfasie <fasie.madalin@gmail.com>
2022-01-12 07:57:39 +0200

    implement prototype pattern

---
+++ Creational/Prototype/prototype.py
@@ -0,0 +1,127 @@
+"""
+The technology advanced so much until year 2079 that
+cloning inanimate objects is now possible.
+
+A new restaurant tries to make a profit by cloning its dishes
+instead of recreating them each time.
+"""
+import abc
+import typing as t
+
+
+class DishPrototype(abc.ABC):
+    def clone(self) -> 'DishPrototype':
+        pass
+
+
+class Menu:
+    """ Acts as a prototype registry """
+    def __init__(self):
+        self.items: t.Dict[str, DishPrototype] = {}
+
+    def add(self, name: str, dish: DishPrototype):
+        self.items[name] = dish
+
+    def remove(self, name):
+        if name in self.items:
+            self.items.pop(name)
+
+    def get_by_name(self, name: str) -> DishPrototype:
+        return self.items[name].clone()
+
+    def get_by_ingredients(self, ingredient: str) -> DishPrototype:
+        for dish in self.items.values():
+            if ingredient in dish.ingredients:
+                return dish.clone()
+
+
+class PizzaPrototype(DishPrototype):
+    def __init__(self, size: int):
+        self.size = size
+        self.ingredients = []
+
+    def _clone(self, prototype: DishPrototype) -> DishPrototype:
+        clone = PizzaPrototype(size=prototype.size)
+        clone.ingredients = prototype.ingredients
+        return clone
+
+    def clone(self) -> DishPrototype:
+        return self._clone(self)
+
+
+class PizzaProsciuttoFunghi(PizzaPrototype):
+    def __init__(self, size: int, extra_mozzarella: bool = False):
+        self.size = size
+        self.extra_mozzarella = extra_mozzarella
+        self.ingredients = ['mozzarella', 'prosciutto', 'mushrooms']
+
+    def plate(self) -> None:
+        text = f'Serving a pizza prosciutto e fungi of size {self.size}'
+        if self.extra_mozzarella:
+            text += ' with extra mozzarella'
+
+        print(text)
+
+    def _clone(self, prototype: PizzaPrototype) -> PizzaPrototype:
+        clone = PizzaProsciuttoFunghi(size=prototype.size)
+        clone.extra_mozzarella = prototype.extra_mozzarella
+        clone.ingredients = prototype.ingredients
+        return clone
+
+    def clone(self) -> PizzaPrototype:
+        return self._clone(self)
+
+
+class SpaghettiCarbonara(DishPrototype):
+    def __init__(self, weight: int = 300, extra_parmigiano: bool = False):
+        self.weight = weight
+        self.extra_parmigiano = extra_parmigiano
+        self.ingredients = ['spaghetti', 'egg', 'pancetta']
+
+    def plate(self) -> None:
+        text = f'Serving a spagetti carbonara {self.weight}g'
+        if self.extra_parmigiano:
+            text += ' with extra parmigiano'
+
+        print(text)
+
+    def _clone(self, prototype: DishPrototype) -> DishPrototype:
+        clone = SpaghettiCarbonara(
+            weight=self.weight,
+            extra_parmigiano=self.extra_parmigiano)
+        clone.ingredients = prototype.ingredients
+        return clone
+
+    def clone(self) -> DishPrototype:
+        return self._clone(self)
+
+
+class Restaurant:
+    def __init__(self):
+        self.order = []
+        self.menu = Menu()
+
+    def build_menu(self) -> None:
+        pizza = PizzaProsciuttoFunghi(size=32, extra_mozzarella=True)
+        spaghetti = SpaghettiCarbonara(weight=400)
+
+        self.menu.add('Prosciutto e Funghi', pizza)
+        self.menu.add('Spaghetti Carbonara', spaghetti)
+
+    def build_order(self) -> None:
+        self.order.append(self.menu.get_by_name('Prosciutto e Funghi'))
+        self.order.append(self.menu.get_by_name('Spaghetti Carbonara'))
+        self.order.append(self.menu.get_by_ingredients('mozzarella'))
+
+    def serve(self) -> None:
+        for dish in self.order:
+            dish.plate()
+
+        self.order = []
+
+
+if __name__ == '__main__':
+    restaurant = Restaurant()
+    restaurant.build_menu()
+    restaurant.build_order()
+    restaurant.serve()
---
+++ Creational/Prototype/prototype_simple.py
@@ -0,0 +1,80 @@
+"""
+The technology advanced so much until year 2079 that
+cloning inanimate objects is now possible.
+
+A new restaurant tries to make a profit by cloning its dishes
+instead of recreating them each time.
+"""
+import abc
+
+
+class DishPrototype(abc.ABC):
+    def clone(self):
+        pass
+
+
+class PizzaProsciuttoFunghi(DishPrototype):
+    def __init__(self, prototype: DishPrototype = None):
+        self.size: int = None
+        self.extra_mozzarella: bool = False
+
+        if prototype:
+            self.size = prototype.size
+            self.extra_mozzarella = prototype.extra_mozzarella
+
+    def plate(self) -> None:
+        text = f'Serving a pizza prosciutto e fungi of size {self.size}'
+        if self.extra_mozzarella:
+            text += ' with extra mozzarella'
+
+        print(text)
+
+    def clone(self) -> DishPrototype:
+        return PizzaProsciuttoFunghi(self)
+
+
+class SpaghettiCarbonara(DishPrototype):
+    def __init__(self, weight: int = 300, extra_parmigiano: bool = False):
+        self.weight = weight
+        self.extra_parmigiano = extra_parmigiano
+
+    def plate(self) -> None:
+        text = f'Serving a spagetti carbonara {self.weight}g'
+        if self.extra_parmigiano:
+            text += ' with extra parmigiano'
+
+        print(text)
+
+    def clone(self) -> DishPrototype:
+        return SpaghettiCarbonara(
+            weight=self.weight,
+            extra_parmigiano=self.extra_parmigiano)
+
+
+class Restaurant:
+    def __init__(self):
+        self.order = []
+
+    def build_order(self) -> None:
+        pizza = PizzaProsciuttoFunghi()
+        pizza.size = 32
+        pizza.extra_mozzarella = True
+
+        self.order.append(pizza)
+        self.order.append(pizza.clone())
+
+        spaghetti = SpaghettiCarbonara(weight=400)
+        self.order.append(spaghetti)
+        self.order.append(spaghetti.clone())
+
+    def serve(self) -> None:
+        for dish in self.order:
+            dish.plate()
+
+        self.order = []
+
+
+if __name__ == '__main__':
+    restaurant = Restaurant()
+    restaurant.build_order()
+    restaurant.serve()
